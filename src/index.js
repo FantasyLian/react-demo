12b919b63a87520e50cbfa082be9afad4ab6aaa0
Mr.cat <fantasy.lian@163.com>
2021-04-06 11:04:18 +0800

    recat app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import 'normalize.css';
+import './styles/index.css';
+import Home from './home/home.js';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>,
   document.getElementById('root')
 );
