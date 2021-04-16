a7039bf64a862ae8f125b93d437b5ae13def4cc9
Mr.cat <fantasy.lian@163.com>
2021-04-16 09:40:37 +0800

    增加页面

--- src/views/login/login.js
+++ src/views/login/login.js
@@ -1,68 +1,93 @@
-import React from 'react'
-import { Form, Input, Button, Checkbox } from 'antd'
-import { UserOutlined, LockOutlined } from '@ant-design/icons'
-import './login.less'
+import React, { Component } from 'react'
+import Particles from 'react-particles-js'
+import { Form, Icon, Input, Button } from 'antd'
+import { connect } from 'react-redux'
+import { setUserInfo } from '@/redux/actions/userInfo'
+import '@/assets/styles/login.less'
 
-// 登录表单
-const LoginForm = () => {
-  const onFinish = (values) => {
-    console.log('Received values of form: ', values)
+const FormItem = Form.Item
+class Login extends Component {
+  state = { clientHeight: document.documentElement.clientHeight || document.body.clientHeight };
+  constructor(props) {
+    super(props)
+    this.onResize = this.onResize.bind(this)
   }
-  return (
-    <Form
-      initialValues={{
-        remember: true,
-      }}
-      onFinish={onFinish}
-    >
-      <Form.Item
-        name="用户名"
-        rules={[
-          {
-            required: true,
-            message: '请输入用户名!',
-          },
-        ]}
-      >
-        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="请输入用户名" />
-      </Form.Item>
-      <Form.Item
-        name="密码"
-        rules={[
-          {
-            required: true,
-            message: '请输入密码!',
-          },
-        ]}
-      >
-        <Input
-          prefix={<LockOutlined className="site-form-item-icon" />}
-          type="password"
-          placeholder="请输入密码"
-        />
-      </Form.Item>
-      <Form.Item>
-        <Form.Item name="remember" valuePropName="checked" noStyle>
-          <Checkbox>记住我</Checkbox>
-        </Form.Item>
-
-        <a className="login-form-forgot" href="##">忘记密码</a>
-      </Form.Item>
-
-      <Form.Item>
-        <Button type="primary" htmlType="submit" className="login-form-button">
-          登录
-        </Button> 或 <a href="#/register">立即注册!</a>
-      </Form.Item>
-    </Form>
-  )
-}
-export default class Login extends React.Component {
+  login = e => {
+    e.preventDefault()
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        localStorage.setItem('isLogin', '1')
+        // 模拟生成一些数据
+        this.props.setUserInfo(Object.assign({}, values, { role: { type: 1, name: '超级管理员' } }))
+        localStorage.setItem('userInfo', JSON.stringify(Object.assign({}, values, { role: { type: 1, name: '超级管理员' } })))
+        this.props.history.push('/dashboard')
+      } else {
+        console.log(err)
+      }
+    })
+  };
+  componentDidMount () {
+    window.addEventListener('resize', this.onResize)
+  }
+  componentWillUnmount () {
+    window.addEventListener('resize', this.onResize)
+    // componentWillMount进行异步操作时且在callback中进行了setState操作时，需要在组件卸载时清除state
+    this.setState = () => {
+      return
+    }
+  }
+  onResize () {
+    this.setState({ clientHeight: document.documentElement.clientHeight || document.body.clientHeight })
+  }
   render () {
+    const { getFieldDecorator } = this.props.form
     return (
-      <div className="page-wrap">
-        <LoginForm />
+      <div className="container">
+        <Particles
+          height={this.state.clientHeight - 5 + 'px'}
+          params={{
+            number: { value: 50 },
+            ize: { value: 3 },
+            interactivity: {
+              events: {
+                onhover: { enable: true, mode: 'repulse' }
+              }
+            }
+          }}
+        />
+        <div className="content">
+          <div className="title">后台管理系统</div>
+          <Form className="login-form">
+            <FormItem>
+              {getFieldDecorator('userName', {
+                rules: [{ required: true, message: '请填写用户名！' }]
+              })(<Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="用户名" />)}
+            </FormItem>
+            <FormItem>
+              {getFieldDecorator('password', {
+                rules: [{ required: true, message: '请填写密码！' }]
+              })(<Input.Password prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="密码" />)}
+            </FormItem>
+            <FormItem>
+              <Button type="primary" htmlType="submit" block onClick={this.login}>
+                登录
+							</Button>
+              <div style={{ color: '#999', paddingTop: '10px', textAlign: 'center' }}>Tips : 输入任意用户名密码即可</div>
+            </FormItem>
+          </Form>
+        </div>
       </div>
     )
   }
 }
+
+const mapStateToProps = state => state
+const mapDispatchToProps = dispatch => ({
+  setUserInfo: data => {
+    dispatch(setUserInfo(data))
+  }
+})
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Form.create()(Login))
